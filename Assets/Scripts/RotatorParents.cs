a635471bf179189722a553a0dbbeb5e791a04687
Kraig Warnock <warnockr@shernet.sheridancollege.ca>
2025-01-16 12:59:31 -0500

    Parental Changes

--- Assets/Scripts/RotatorParents.cs
+++ Assets/Scripts/RotatorParents.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetAKeyPress : MonoBehaviour
+public class RotatorParents : MonoBehaviour
 {
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +14,6 @@ public class GetAKeyPress : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Space!");
-        }
+        transform.Rotate(0, 0, speed * Time.deltaTime);
     }
 }
