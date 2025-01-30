4af46fdd511b00e3317fc073eab3c66ad8e71006
DESKTOP-28FP8BD\Crag <warnockr@sheridancollege.ca>
2025-01-29 23:15:55 -0500

    Adding the assets needed and scripts to move objects accordingly

--- Assets/Scripts/Smoke.cs
+++ Assets/Scripts/Smoke.cs
@@ -1,30 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
-public class Flicker : MonoBehaviour
+public class Smoke : MonoBehaviour
 {
-    public AnimationCurve slowCurve;
+    public Vector2 start;
+    public Vector3 end;
+    [Range(0, 1)]
+    public float t;
 
-    [Range(0,1)]
-        public float t;
-    public float scale;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
-
+        t += Time.deltaTime/2;
         if (t > 1)
         {
             t = 0;
         }
-        transform.localScale = Vector2.one * slowCurve.Evaluate(t);
+        transform.position = Vector2.Lerp(start, end, t);
     }
 }
